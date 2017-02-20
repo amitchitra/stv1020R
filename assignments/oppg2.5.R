49add87a7d48b6456a1d499f8a16658d87a67120
martigso <m.g.soyland@stv.uio.no>
2017-02-20 13:59:01 +0100

    Lagt til flere oppgaver på 2.5 -- skille mellom må, bør og kan gjøre

--- assignments/oppg2.5.R
+++ assignments/oppg2.5.R
@@ -1,8 +1,26 @@
 # Oppgaver 2.5
 # Husk å kommentere koden
 
-# 1. Last ned og inn "avengers.csv" fra https://github.com/martigso/stv1020R/tree/master/data
+# Du har fått jobb som vitenskapelig assistent hos Peter Langsæther, som er veldig opptatt av Avengers-helter
+# Han har gitt deg i oppgave å finne data på disse heltene og gi ham interessant deskriptiv statistikk på dem.
+# Du finner ut at det er data tilgjengelig på https://github.com/martigso/stv1020R/tree/master/data
+# Disse dataene inkluderer navn på helt, hvor mange år de har vært med, hvor mange episoder de har vært med i osv.
+
+# Må gjøres:
+# 1. Last ned og inn "avengers.csv"
 
 # 2. Lag en ny variabel i data som indikerer om en avengershelt har vært med i mer enn 1000 episoder
 
 # 3. Lag et box-plot av to valgfrie variabler i "avengers"-data
+
+# Bør gjøres:
+# 4. Viser kjikvadrat-test signifikant sammenheng mellom variabelen i avengers-data fra oppgave 2 og "gender"?
+
+# 5. Lag et plot som viser verdiene på "year" på x-aksen og verdiene på "apperances" på y-aksen
+
+# 6. Finn Persons-r mellom variabelen "year og variabelen "apperances". Skriv kort tolkning av resultatet.
+
+# Kan gjøres:
+# 7. Lag et subsett av data med bare nåværende helter (avengers$current == TRUE)
+
+# 8. Gir kjikvadrattesten fra oppgave 4 samme resultat på de reduserte dataene?
