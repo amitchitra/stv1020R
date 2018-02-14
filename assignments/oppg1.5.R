639d983b1291856b9b77a74b8cc5e4c93c7398d9
erlendlangorgen <erlend.langorgen@stv.uio.no>
2018-02-14 15:31:42 +0100

    Fjerner filer som er flyttet til 2017

--- assignments/oppg1.5.R
+++
@@ -1,7 +0,0 @@
-# Repetisjonsoppgaver:
-
-# 1. Lag en vektor som inneholder navnet på 10 professorer på UiO
-
-# 2. Lag en vektor som indikerer kjønnet på professorene fra oppgave 1
-
-# 3. Få R til å gi deg frekvensfordelingen av kjønn
